a6a1d65d023e92b1fe74bd6456625256a86d7eb4
KvnCncpcion <kvncncpcion@gmail.com>
2022-11-19 21:48:23 +0800

    Adding Import Tkinter

--- Encrypting and Decrypting.py
+++ Encrypting and Decrypting.py
@@ -1,3 +1,8 @@
 print("=================== PROGRAMMED BY ======================")
 print("============= KEVIN JOSEPH G. CONCEPCION ===============")
 print("===================== BSCOE 2-2 ========================")
+
+from tkinter import *
+import base64
+from tkinter import messagebox
+import tkinter.font as font
