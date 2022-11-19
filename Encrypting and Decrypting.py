fa2f65125a8fa597b6cb47cca264de733662fc97
KvnCncpcion <kvncncpcion@gmail.com>
2022-11-19 21:59:24 +0800

    Defining Encode for Encryption Function

--- Encrypting and Decrypting.py
+++ Encrypting and Decrypting.py
@@ -6,3 +6,13 @@ from tkinter import *
 import base64
 from tkinter import messagebox
 import tkinter.font as font
+
+# Defining Encode for Encryption
+def encode(key, msg):
+    encrypt = []
+    for i in range(len(msg)):
+        list_key = key[i % len(key)]
+        list_enc = chr((ord(msg[i]) + ord(list_key)) % 256)
+        encrypt.append(list_enc)
+
+    return base64.urlsafe_b64encode("".join(encrypt).encode()).decode()
